bc722d484c226189ea825d22a2c5434c2936f680
vitlog7446-star <vitlog.7446@gmail.com>
2026-09-22 11:54:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from playwright.sync_api import sync_playwright
 import time
 from pathlib import Path
+import os
 
 pasta_projeto = Path(__file__).resolve().parent
 arquivo_csv = pasta_projeto / "relatorio.csv"
 
-dominio = 'vit'
-cpf = '05155209005'
-user = 'edusilva'
-senha = '1008'
+dominio = os.getenv("SSW_DOMINIO")
+cpf = os.getenv("SSW_CPF")
+user = os.getenv("SSW_USUARIO")
+senha = os.getenv("SSW_SENHA")
 
 def baixar_relatorio():
     with sync_playwright() as pw:
@@ -30,10 +31,8 @@ def baixar_relatorio():
             with page.expect_navigation():
                 page.get_by_role("link", name="►").click()
 
-            page.wait_for_load_state("networkidle")
-
             campo_opcao = page.locator('[id="3"]').last
-            campo_opcao.wait_for(state="visible")
+            campo_opcao.wait_for(state="visible", timeout=60000)
             
             campo_opcao.click()
             campo_opcao.fill("063")
